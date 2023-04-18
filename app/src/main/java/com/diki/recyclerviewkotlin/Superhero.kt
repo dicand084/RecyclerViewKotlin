b10f3d4060b9239fce2a44b2f5ae37890b62faab
M S I <dicand82@gmail.com>
2023-04-19 00:21:53 +0700

    update

--- app/src/main/java/com/diki/recyclerviewkotlin/Superhero.kt
+++ app/src/main/java/com/diki/recyclerviewkotlin/Superhero.kt
@@ -1,7 +1,11 @@
 package com.diki.recyclerviewkotlin
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Superhero(
     val imgSuperhero : Int,
     val nameSuperhero: String,
     val descSuperhero: String
-)
+):Parcelable
---
+++ app/src/main/java/com/diki/recyclerviewkotlin/SuperheroAdapter.kt
@@ -0,0 +1,4 @@
+package com.diki.recyclerviewkotlin
+
+class SuperheroAdapter {
+}
