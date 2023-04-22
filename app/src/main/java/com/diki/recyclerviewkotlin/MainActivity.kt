d7c9bc21868ca6131b62dbd1cb7d1109c8597d52
M S I <dicand82@gmail.com>
2023-04-23 04:17:19 +0700

    update

--- app/src/main/java/com/diki/recyclerviewkotlin/MainActivity.kt
+++ app/src/main/java/com/diki/recyclerviewkotlin/MainActivity.kt
@@ -2,10 +2,64 @@ package com.diki.recyclerviewkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val superheroList = listOf<Superhero>(
+            Superhero(
+                R.drawable.ant,
+                "Ant-mant",
+                "Ant-Man dan Tawon menemukan diri mereka menjelajahi Quantum Realm, berinteraksi dengan makhluk baru yang aneh dan memulai petualangan yang mendorong mereka melampaui batas dari apa yang mereka pikir mungkin."
+            ),
+            Superhero(
+                R.drawable.ant,
+                "Ant-mant",
+                "Ant-Man dan Tawon menemukan diri mereka menjelajahi Quantum Realm, berinteraksi dengan makhluk baru yang aneh dan memulai petualangan yang mendorong mereka melampaui batas dari apa yang mereka pikir mungkin."
+            ),Superhero(
+                R.drawable.ant,
+                "Ant-mant",
+                "Ant-Man dan Tawon menemukan diri mereka menjelajahi Quantum Realm, berinteraksi dengan makhluk baru yang aneh dan memulai petualangan yang mendorong mereka melampaui batas dari apa yang mereka pikir mungkin."
+            ),Superhero(
+                R.drawable.ant,
+                "Ant-mant",
+                "Ant-Man dan Tawon menemukan diri mereka menjelajahi Quantum Realm, berinteraksi dengan makhluk baru yang aneh dan memulai petualangan yang mendorong mereka melampaui batas dari apa yang mereka pikir mungkin."
+            ),Superhero(
+                R.drawable.ant,
+                "Ant-mant",
+                "Ant-Man dan Tawon menemukan diri mereka menjelajahi Quantum Realm, berinteraksi dengan makhluk baru yang aneh dan memulai petualangan yang mendorong mereka melampaui batas dari apa yang mereka pikir mungkin."
+            ),Superhero(
+                R.drawable.ant,
+                "Ant-mant",
+                "Ant-Man dan Tawon menemukan diri mereka menjelajahi Quantum Realm, berinteraksi dengan makhluk baru yang aneh dan memulai petualangan yang mendorong mereka melampaui batas dari apa yang mereka pikir mungkin."
+            ),Superhero(
+                R.drawable.ant,
+                "Ant-mant",
+                "Ant-Man dan Tawon menemukan diri mereka menjelajahi Quantum Realm, berinteraksi dengan makhluk baru yang aneh dan memulai petualangan yang mendorong mereka melampaui batas dari apa yang mereka pikir mungkin."
+            ),Superhero(
+                R.drawable.ant,
+                "Ant-mant",
+                "Ant-Man dan Tawon menemukan diri mereka menjelajahi Quantum Realm, berinteraksi dengan makhluk baru yang aneh dan memulai petualangan yang mendorong mereka melampaui batas dari apa yang mereka pikir mungkin."
+            ),Superhero(
+                R.drawable.ant,
+                "Ant-mant",
+                "Ant-Man dan Tawon menemukan diri mereka menjelajahi Quantum Realm, berinteraksi dengan makhluk baru yang aneh dan memulai petualangan yang mendorong mereka melampaui batas dari apa yang mereka pikir mungkin."
+            ),Superhero(
+                R.drawable.ant,
+                "Ant-mant",
+                "Ant-Man dan Tawon menemukan diri mereka menjelajahi Quantum Realm, berinteraksi dengan makhluk baru yang aneh dan memulai petualangan yang mendorong mereka melampaui batas dari apa yang mereka pikir mungkin."
+            ),
+        )
+
+        val recyclerView = findViewById<RecyclerView>(R.id.rv_hero)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = SuperheroAdapter(this, superheroList){
+
+        }
     }
 }
